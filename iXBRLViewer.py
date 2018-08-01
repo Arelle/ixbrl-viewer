9192a8e38e269345ef60d677934359ab485829ba
Paul Warren <pdw@ex-parrot.com>
2018-08-01 23:24:54 +0100

    Rework concept data into hash, 1:1 prefix/ns map

--- iXBRLViewer.py
+++ iXBRLViewer.py
@@ -5,6 +5,40 @@ import io
 import os
 import jinja2
 
+class NamespaceMap:
+    """Class for building a 1:1 map of prefixes to namespace URIs
+
+    Will attempt to use a provided, preferred prefix, but will uniquify as
+    required.
+    """
+
+    def __init__(self):
+        self.nsmap = dict()
+        self.prefixmap = dict()
+
+    def getPrefix(self, ns, preferredPrefix = None):
+        """Get the prefix for the specified namespace.
+
+        If no prefix is yet defined, define one using the preferred prefix, if
+        provided and not yet used, otherwise add a number to the end of the preferred
+        prefix (or the string "ns")
+        """
+    
+        prefix = self.nsmap.get(ns, None)
+        if not prefix:
+            if preferredPrefix and preferredPrefix not in self.prefixmap:
+                prefix = preferredPrefix
+            else:
+                p = preferredPrefix if preferredPrefix else "ns";
+                n = 0
+                while "%s%d" % (p,n) in self.prefixmap:
+                    n += 1
+                
+                prefix = "%s%d" % (p,n)
+                    
+            self.prefixmap[prefix] = ns
+            self.nsmap[ns] = prefix
+        return prefix
 
 def lineWrap(s, n = 80):
     return "\n".join([s[i:i+n] for i in range(0, len(s), n)])
@@ -16,47 +50,41 @@ def lineWrap(s, n = 80):
 def saveViewer(dts, outFile):
     idGen = 0
     taxonomyData = {
-        "concepts": [],
+        "concepts": {},
         "facts": {},
     }
+    nsmap = NamespaceMap()
     for f in dts.facts:
         if f.id is None:
             f.set("id","ixv-%d" % (idGen))
         idGen += 1
+        conceptName = "%s:%s" % (nsmap.getPrefix(f.qname.namespaceURI, f.qname.prefix), f.qname.localName)
         taxonomyData["facts"][f.id] = {
             "format": str(f.format),
             "value": f.value,
+            "concept": conceptName,
         }
         labelsRelationshipSet = dts.relationshipSet(XbrlConst.conceptLabel)
         #dts.info("info:iXBRLViewer", "Fact: %s" % f.concept.qname)
         labels = labelsRelationshipSet.fromModelObject(f.concept)
-        conceptData = {
-            "name": f.qname.localName,
-            "ns": f.qname.prefix,
-            "labels": []
-        }
-        for lr in labels:
-            l = lr.toModelObject
-            conceptData["labels"].append({
-                "lang": l.xmlLang,
-                "text": l.textValue,
-                "role": l.role,
-            })
-            #dts.info("info:iXBRLViewer", "%s '%s'" %  (l.xmlLang, l.textValue))
-    
-        taxonomyData["concepts"].append(conceptData)
 
-    #headElt = XmlUtil.descendants(dts.modelDocument.xmlRootElement,"*", "head")
-    #XmlUtil.addChild(
-    #    headElt[0],    
-    #    XbrlConst.xhtml, 
-    #    "script", 
-    #    attributes = { "type": "application/json" },
-    #    text=json.dumps(taxonomyData, indent=1)
-    #)
+        if conceptName not in taxonomyData["concepts"]:
+            conceptData = {
+                "labels": []
+            }
+            for lr in labels:
+                l = lr.toModelObject
+                conceptData["labels"].append({
+                    "lang": l.xmlLang,
+                    "text": l.textValue,
+                    "role": l.role,
+                })
+        
+            taxonomyData["concepts"][conceptName] = conceptData
 
-    #taxonomyDataJSON = json.dumps(taxonomyData, indent=1)
-    taxonomyDataJSON = json.dumps(taxonomyData, indent=None, separators=(",",":"))
+    taxonomyData["prefixes"] = nsmap.prefixmap
+    taxonomyDataJSON = json.dumps(taxonomyData, indent=1)
+    #taxonomyDataJSON = json.dumps(taxonomyData, indent=None, separators=(",",":"))
 
     dts.info("viewer:info", "Saving iXBRL viewer to %s" % (outFile))
     xml = io.StringIO()

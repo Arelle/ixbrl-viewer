0a75759ba1d0d2979d15944aedf59d8f2c4c8d05
Paul Warren <pdw@ex-parrot.com>
2021-10-28 23:07:31 +0100

    Additional tests.

--- iXBRLViewerPlugin/viewer/src/js/outline.test.js
+++ iXBRLViewerPlugin/viewer/src/js/outline.test.js
@@ -1,4 +1,4 @@
-// Copyright 2019 Workiva Inc.
+// Copyright 2021 Workiva Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -98,18 +98,63 @@ addTestDimension(testReportData, "Dimension 2");
 const testFacts =  {
         f1: {
             a: {
-                c: "eg:LineItem1"
+                c: "eg:LineItem1",
+                p: "2019-01-01"
             }
         },
         f2: {
             a: {
-                c: "eg:LineItem2"
+                c: "eg:LineItem2",
+                p: "2019-01-01"
             }
         },
         f3: {
             a: {
                 c: "eg:LineItem1",
+                p: "2019-01-01",
                 "eg:Dimension1": "eg:Member1"
+            }
+        },
+        f1a: {
+            a: {
+                c: "eg:LineItem1",
+                p: "2020-01-01"
+            }
+        },
+        f1b: {
+            a: {
+                c: "eg:LineItem1",
+                p: "2021-01-01"
+            }
+        },
+        f1c: {
+            a: {
+                c: "eg:LineItem1",
+                p: "2022-01-01"
+            }
+        },
+        f1d: {
+            a: {
+                c: "eg:LineItem1",
+                p: "2023-01-01"
+            }
+        },
+        f2a: {
+            a: {
+                c: "eg:LineItem2",
+                p: "2020-01-01"
+            }
+        },
+        f2b: {
+            a: {
+                c: "eg:LineItem2",
+                p: "2021-01-01"
+            }
+        },
+        f2c: {
+            a: {
+                c: "eg:LineItem2",
+                p: "2022-01-01"
             }
         },
     };
@@ -130,6 +175,7 @@ function testReport(factList) {
 }
 
 describe("Section filtering", () => {
+    // f1 has a line item from elr1, f2 has a line item from elr3
     test("Both groups", () => {
         var report = testReport(["f1", "f2"]);
         var outline = new DocumentOutline(report);
@@ -162,13 +208,17 @@ describe("Dimensional filtering", () => {
         var report = testReport(["f3"]);
         var outline = new DocumentOutline(report);
         // ELR1 does not mention Dimension 1, so f3 is included.
-        expect(outline.factInGroup(report.getItemById("f3"), "elr1")).toBe(true);
+        var f3 = report.getItemById("f3");
+        expect(outline.factInGroup(f3, "elr1")).toBe(true);
         // ELR1 includes Dimension 1 with specified member, so included
-        expect(outline.factInGroup(report.getItemById("f3"), "elr2")).toBe(true);
-        expect(outline.factInGroup(report.getItemById("f3"), "elr3")).toBe(false);
+        expect(outline.factInGroup(f3, "elr2")).toBe(true);
+        expect(outline.factInGroup(f3, "elr3")).toBe(false);
 
         expect(outline.sortedSections()).toEqual(["elr1", "elr2"]);
 
+        expect(outline.groupsForFact(f3)).toEqual(["elr1", "elr2"]);
+
+        // Exclude Member1 from Dimension1 in ELR2
         report.data.rels.pres.elr2['eg:Dimension1'] = [
             { "t": "eg:Member2" }
         ];
@@ -177,13 +227,103 @@ describe("Dimensional filtering", () => {
         outline = new DocumentOutline(report);
 
         // ELR1 does not mention Dimension 1, so f3 is included.
-        expect(outline.factInGroup(report.getItemById("f3"), "elr1")).toBe(true);
+        expect(outline.factInGroup(f3, "elr1")).toBe(true);
         // ELR1 includes Dimension 1 but specified member is now removed
-        expect(outline.factInGroup(report.getItemById("f3"), "elr2")).toBe(false);
-        expect(outline.factInGroup(report.getItemById("f3"), "elr3")).toBe(false);
+        expect(outline.factInGroup(f3, "elr2")).toBe(false);
+        expect(outline.factInGroup(f3, "elr3")).toBe(false);
 
         expect(outline.sortedSections()).toEqual(["elr1"]);
+        expect(outline.groupsForFact(f3)).toEqual(["elr1"]);
 
     });
 
 });
+
+describe("Section grouping", () => {
+    // f1* in ELR1, f2* in ELR3
+    test("Single group", () => {
+        // All facts in a single group
+        var report = testReport(["f1", "f1a"]);
+        var outline = new DocumentOutline(report);
+        expect(outline.sortedSections()).toEqual(["elr1"]);
+        expect(outline.sections["elr1"].id).toEqual("f1");
+        expect(outline.sections["elr3"]).toBeUndefined();
+    });
+
+    test("Longest runs 1", () => {
+        // ELR1 is the f1, f1a run
+        // ELR3 is the f2a, f2b run
+        var report = testReport(["f1", "f1a", "f2", "f1b", "f2a", "f2b"]);
+        var outline = new DocumentOutline(report);
+        expect(outline.sortedSections()).toEqual(["elr1", "elr3"]);
+        expect(outline.sections["elr1"].id).toEqual("f1");
+        expect(outline.sections["elr3"].id).toEqual("f2a");
+    });
+
+    test("Longest runs 2", () => {
+        // ELR1 is the f1a, f1b run
+        // ELR3 is the f2, f2b run
+        var report = testReport(["f1", "f2", "f2b", "f1a", "f1b", "f2a"]);
+        var outline = new DocumentOutline(report);
+        expect(outline.sortedSections()).toEqual(["elr1", "elr3"]);
+        expect(outline.sections["elr1"].id).toEqual("f1a");
+        expect(outline.sections["elr3"].id).toEqual("f2");
+    });
+
+    test("Equal length runs", () => {
+        // If multiple runs have the same length, we arbitrarily assign the ELR
+        // to the first.
+        var report = testReport(["f1", "f1a", "f2", "f1b", "f1c", "f2b" ]);
+        var outline = new DocumentOutline(report);
+        expect(outline.sortedSections()).toEqual(["elr1", "elr3"]);
+        expect(outline.sections["elr1"].id).toEqual("f1");
+        expect(outline.sections["elr3"].id).toEqual("f2");
+    });
+
+    test("Hidden facts", () => {
+        // Start with ELR1*2 ELR3*1 ELR1*3
+        var report = testReport(["f1a", "f1", "f2", "f1b", "f1c", "f1d"]);
+        var outline = new DocumentOutline(report);
+        expect(outline.sortedSections()).toEqual(["elr1", "elr3"]);
+        expect(outline.sections["elr1"].id).toEqual("f1b");
+        expect(outline.sections["elr3"].id).toEqual("f2");
+
+        // Make f1c hidden.  We now have ELR1*2 ELR3*1 ELR1*2
+        // The first ELR1 run should be selected.
+        report.getItemById("f1c").ixNode.wrapperNode = $('');
+        outline = new DocumentOutline(report);
+        expect(outline.sortedSections()).toEqual(["elr1", "elr3"]);
+        expect(outline.sections["elr1"].id).toEqual("f1a");
+        expect(outline.sections["elr3"].id).toEqual("f2");
+
+        // Make f1a hidden.  f1b-[f1c]-f1d is now the longest run.
+        report.getItemById("f1a").ixNode.wrapperNode = $('');
+        outline = new DocumentOutline(report);
+        expect(outline.sortedSections()).toEqual(["elr1", "elr3"]);
+        expect(outline.sections["elr1"].id).toEqual("f1b");
+        expect(outline.sections["elr3"].id).toEqual("f2");
+
+        // Hide f2.  We now have a single run for ELR1
+        report.getItemById("f2").ixNode.wrapperNode = $('');
+        outline = new DocumentOutline(report);
+        expect(outline.sortedSections()).toEqual(["elr1"]);
+        expect(outline.sections["elr1"].id).toEqual("f1");
+
+    });
+});
+
+describe("No presentation", () => {
+    // f1 has a line item from elr1, f2 has a line item from elr3
+    test("Remove presentation relationships", () => {
+        var report = testReport(["f1", "f2"]);
+        var outline = new DocumentOutline(report);
+        expect(outline.sortedSections()).toEqual(["elr1", "elr3"]);
+        expect(outline.hasOutline()).toEqual(true);
+
+        delete report.data.rels.pres;
+        outline = new DocumentOutline(report);
+        expect(outline.sortedSections()).toEqual([]);
+        expect(outline.hasOutline()).toEqual(false);
+    });
+});
+

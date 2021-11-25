aaec566438a9fcfd61d3f30f4dcee550fc840749
Paul Warren <pdw@ex-parrot.com>
2021-11-25 20:55:18 +0000

    Add missing config file, remove input files from command line.

---
+++ iXBRLViewerPlugin/viewer/i18next-parser.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    input: [
+        "src/js/*.js",
+        "src/html/*.html"
+    ],
+    output: "iXBRLViewerPlugin/viewer/src/i18n/$LOCALE/$NAMESPACE.json",
+    locales: [ "en", "es" ]
+};

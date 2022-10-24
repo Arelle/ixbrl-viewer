f43a05407aa0950ca0924bbcd57aeda8e26fd6e3
Paul Warren <pdw@ex-parrot.com>
2022-10-24 22:59:01 +0100

    Switch to class syntax.

--- iXBRLViewerPlugin/viewer/src/js/identifiers.js
+++ iXBRLViewerPlugin/viewer/src/js/identifiers.js
@@ -12,39 +12,38 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var schemes = {
+const schemes = {
     "http://standards.iso.org/iso/17442": { "name": "LEI", "url": "https://search.gleif.org/#/record/%s" },
     "http://www.sec.gov/CIK": { "name": "CIK", "url": "https://www.sec.gov/cgi-bin/browse-edgar?CIK=%s"},
     "http://www.companieshouse.gov.uk/": { "name": "UK CRN", "url": "https://beta.companieshouse.gov.uk/company/%08d"},
 };
 
-export function Identifiers() {
-}
-
-Identifiers.identifierURLForFact = function(fact) {
-    var data = schemes[fact.identifier().namespace];
-    if (data !== undefined) {
-        var url = data.url.replace('%s', fact.identifier().localname);
-        url = url.replace(/%0(\d+)d/, function (match, width) { 
-            return fact.identifier().localname.padStart(width, "0");
-        });
-        return url;
+export class Identifiers {
+    static identifierURLForFact(fact) {
+        const data = schemes[fact.identifier().namespace];
+        if (data !== undefined) {
+            let url = data.url.replace('%s', fact.identifier().localname);
+            url = url.replace(/%0(\d+)d/, function (match, width) { 
+                return fact.identifier().localname.padStart(width, "0");
+            });
+            return url;
+        }
+        return undefined;
     }
-    return undefined;
-}
 
-Identifiers.identifierNameForFact = function(fact) {
-    var data = schemes[fact.identifier().namespace];
-    if (data !== undefined) {
-        return data.name;
+    static identifierNameForFact(fact) {
+        const data = schemes[fact.identifier().namespace];
+        if (data !== undefined) {
+            return data.name;
+        }
+        return undefined;
     }
-    return undefined;
-}
 
-Identifiers.readableName = function (identifier) {
-    var data = schemes[identifier.namespace];
-    if (data !== undefined) {
-        return "[" + data.name + "] " + identifier.localname;
+    static readableName(identifier) {
+        const data = schemes[identifier.namespace];
+        if (data !== undefined) {
+            return "[" + data.name + "] " + identifier.localname;
+        }
+        return identifier.qname
     }
-    return identifier.qname
 }

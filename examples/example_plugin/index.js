f6a5821726bde71450d765e3264f505c013347d5
Sage Smith <sage.smith@workiva.com>
2023-02-10 12:58:23 -0700

    Actually wait for document to finish loading

--- examples/example_plugin/index.js
+++ examples/example_plugin/index.js
@@ -1,9 +1,11 @@
 import {iXBRLViewer} from "ixbrl-viewer"
 import {ExtendedViewer} from "./extended-viewer.js";
 
-document.addEventListener("DOMContentLoaded", () =>  {
-    const iv = new iXBRLViewer(); // prefer const then let
-    const ivp = new ExtendedViewer(iv);
-    iv.registerPlugin(ivp);
-    iv.load();
-});
+if (document.readyState === 'loading') {  // Loading hasn't finished yet
+    document.addEventListener("DOMContentLoaded", () =>  {
+        const iv = new iXBRLViewer(); // prefer const then let
+        const ivp = new ExtendedViewer(iv);
+        iv.registerPlugin(ivp);
+        iv.load();
+    });
+}

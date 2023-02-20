a92173aa66c90747b7e3d1b3ad065bf0d77187dc
Paul Warren <pdw@ex-parrot.com>
2023-02-20 21:37:13 +0000

    Add custom XHTML serializer.

--- iXBRLViewerPlugin/xhtmlserialize.py
+++ iXBRLViewerPlugin/xhtmlserialize.py
@@ -13,29 +13,125 @@
 # limitations under the License.
 
 from lxml import etree
+from enum import Enum
 import re
+
+XHTML_NS = 'http://www.w3.org/1999/xhtml'
+
+class EscapeMode(Enum):
+    DEFAULT = 0
+    STYLE = 1
 
 class XHTMLSerializer:
 
     # From https://www.w3.org/TR/html401/index/elements.html
-    selfClosableElements = (
+    SELF_CLOSABLE = (
         'area', 'base', 'basefont', 'br', 'col', 'frame', 'hr', 'img', 
         'input', 'isindex', 'link', 'meta', 'param'
     )
 
-    def _expandEmptyTags(self, xml):
-        """
-        Expand self-closing tags
+    ENTITIES = {
+        '>': '&gt;',
+        '<': '&lt;',
+        '"': '&quot;',
+        '&': '&amp;'
+        }
 
-        Self-closing tags cause problems for XHTML documents when treated as
-        HTML.  Tags that are required to be empty (e.g. <br>) are left as
-        self-closing.
-        """
-        for e in xml.iter('*'):
-            m = re.match(r'\{http://www\.w3\.org/1999/xhtml\}(.*)', e.tag)
-            if (m is None or m.group(1) not in XHTMLSerializer.selfClosableElements) and e.text is None:
-                e.text = ''
+    def __init__(self, xml_declaration = True, assume_xhtml = True):
+        self.xml_declaration = xml_declaration
+        self.assume_xhtml = assume_xhtml
 
-    def serialize(self, xmlDocument, fout):
-        self._expandEmptyTags(xmlDocument)
-        fout.write(etree.tostring(xmlDocument, method="xml", encoding="utf-8", xml_declaration=True))
+    def prefix_sort(self, p):
+        return p if p is not None else '0'
+
+    def tag_as_qname(self, tag, nsmap):
+        qname = etree.QName(tag)
+        if qname.namespace is None:
+            return qname.localname
+
+        prefix = next(iter(sorted((p for p, ns in nsmap.items() if ns == qname.namespace), key = self.prefix_sort)))
+
+        if prefix is None:
+            return qname.localname
+        else:
+            return "%s:%s" % (prefix, qname.localname)
+
+    def is_selfclosable(self, n):
+        qname = etree.QName(n)
+        return (
+                qname.localname in XHTMLSerializer.SELF_CLOSABLE
+                and (
+                    qname.namespace == XHTML_NS
+                    or (self.assume_xhtml and qname.namespace is None)))
+
+    def escape_attr(self, s):
+        return re.sub(r'([<>"&])', lambda m: self.ENTITIES[m.group(0)], s)
+
+    def xmlns_declaration(self, prefix, uri):
+        if prefix is None:
+            return 'xmlns="%s"' % self.escape_attr(uri)
+        return 'xmlns:%s="%s"' % (prefix, self.escape_attr(uri))
+
+    def namespace_declarations(self, new_nsmap, cur_nsmap):
+        changed = set(p[0] for p in (set(new_nsmap.items()) ^ set(cur_nsmap.items())))
+        return sorted(self.xmlns_declaration(p, new_nsmap[p]) for p in changed)
+
+    def escape_text(self, s, escape_mode):
+        if s is None:
+            return ''
+        if escape_mode == EscapeMode.STYLE:
+            # Don't escape >
+            # & and < may only appear in a string or comment in CSS, so escape
+            # as if they're in a string.
+            return re.sub(r'([<&])', lambda m: "\\%06X" % ord(m.group(1)), s)
+        return re.sub(r'([<>&])', lambda m: self.ENTITIES[m.group(0)], s)
+
+    def attributes(self, node):
+        return sorted(
+            '%s="%s"' % (self.tag_as_qname(k, node.nsmap), self.escape_attr(v))
+                for k, v in node.items()
+            )
+
+    def write_comment(self, n, escape_mode):
+        return '<!--' + n.text + '-->' + self.escape_text(n.tail, escape_mode)
+
+    def write_processing_instruction(self, n, escape_mode):
+        return '<?' + n.target + ' ' + n.text + '?>' + self.escape_text(n.tail, escape_mode)
+
+    def write_element(self, n, parent_nsmap = {}, escape_mode = EscapeMode.DEFAULT):
+        name = self.tag_as_qname(n.tag, n.nsmap)
+        qname = etree.QName(n.tag)
+        selfclose = len(n) == 0 and n.text is None and self.is_selfclosable(n)
+        parts = [ name ] + self.namespace_declarations(n.nsmap, parent_nsmap) + self.attributes(n)
+        s = '<' + ' '.join(parts)
+
+        if qname.localname.lower() == 'style':
+            inner_escape_mode = EscapeMode.STYLE
+        else:
+            inner_escape_mode = escape_mode
+
+        if selfclose:
+            s += '/>'
+        else:
+            s += '>'
+            s += self.escape_text(n.text, inner_escape_mode)
+
+            for child in n.iterchildren():
+                if isinstance(child, etree._Comment):
+                    s += self.write_comment(child, inner_escape_mode)
+                elif isinstance(child, etree._ProcessingInstruction):
+                    s += self.write_processing_instruction(child, inner_escape_mode)
+                else:
+                    s += self.write_element(child, n.nsmap, inner_escape_mode)
+            s += '</%s>' % name
+
+        s += self.escape_text(n.tail, escape_mode)
+
+        return s
+
+    def serialize(self, element, fout):
+        if hasattr(element, 'getroot'):
+            element = element.getroot()
+        if self.xml_declaration:
+            fout.write('<?xml version="1.0" encoding="utf-8"?>\n'.encode('utf-8'))
+        fout.write(self.write_element(element).encode('utf-8'))
--- tests/unit_tests/iXBRLViewerPlugin/test_xhtmlserialize.py
+++ tests/unit_tests/iXBRLViewerPlugin/test_xhtmlserialize.py
@@ -17,8 +17,7 @@ class TestXHTMLSerializer(unittest.TestCase):
     def _checkTagExpansion(self, html_in, html_out):
         writer = XHTMLSerializer()
         doc = lxml.etree.fromstring(self._html(html_in))
-        writer._expandEmptyTags(doc)
-        self.assertEqual(lxml.etree.tostring(doc, encoding="unicode"), self._html(html_out))
+        self.assertEqual(writer.write_element(doc), self._html(html_out))
 
     def test_expandEmptyTags(self):
         self._checkTagExpansion('<div/>', '<div></div>')
@@ -48,4 +47,93 @@ class TestXHTMLSerializer(unittest.TestCase):
         writer.serialize(doc, f)
 
         # XML declaration should be added.
-        self.assertEqual(f.getvalue().decode('utf-8'), "<?xml version='1.0' encoding='utf-8'?>\n" + htmlsrc)
+        self.assertEqual(f.getvalue().decode('utf-8'), '<?xml version="1.0" encoding="utf-8"?>\n' + htmlsrc)
+
+    def test_custom_xml_serializer(self):
+        tests = (
+            # Self-closable elements should be collapsed
+            (
+                r'''<div><br></br><span></span></div>''',
+                r'''<div><br/><span></span></div>'''
+            ),
+
+            # Non-self-closable element should be expanded
+            (
+                r'''<div><span/></div>''',
+                r'''<div><span></span></div>'''
+            ),
+
+            # > is usually escaped
+            (
+                r'''<div> &lt;foo> &lt;bar&gt; &amp; </div>''',
+                r'''<div> &lt;foo&gt; &lt;bar&gt; &amp; </div>'''
+            ),
+
+            # > is not escaped in a style tag
+            (
+                r'''<style>div &gt; p { color: #777 }</style>''',
+                r'''<style>div > p { color: #777 }</style>''',
+            ),
+
+            # < and & can appear in CSS string
+            (
+                r'''<style>span::before { content: "&lt;&amp;" }</style>''',
+                r'''<style>span::before { content: "\00003C\000026" }</style>''',
+            ),
+
+            # also escaped this way in comments, which is harmless
+            (
+                r'''<style>span { color: #777 } /* &lt;span> elements are styled this way */ </style>''',
+                r'''<style>span { color: #777 } /* \00003Cspan> elements are styled this way */ </style>''',
+            ),
+
+            (
+                r'''<div xmlns="http://www.w3.org/1999/xhtml"><svg xmlns="http://www.w3.org/2000/svg"></svg></div>''',
+                r'''<div xmlns="http://www.w3.org/1999/xhtml"><svg xmlns="http://www.w3.org/2000/svg"></svg></div>''',
+            ),
+            (
+                r'''<xhtml:div xmlns:xhtml="http://www.w3.org/1999/xhtml"><svg xmlns="http://www.w3.org/2000/svg"></svg></xhtml:div>''',
+                r'''<xhtml:div xmlns:xhtml="http://www.w3.org/1999/xhtml"><svg xmlns="http://www.w3.org/2000/svg"></svg></xhtml:div>''',
+            ),
+
+            # Default and prefixed namespace for same URI.  Serialiser prefers unprefixed
+            (
+                r'''<xhtml:div xmlns="http://www.w3.org/1999/xhtml" xmlns:xhtml="http://www.w3.org/1999/xhtml"><svg xmlns="http://www.w3.org/2000/svg"></svg></xhtml:div>''',
+                r'''<div xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/xhtml"><svg xmlns="http://www.w3.org/2000/svg"></svg></div>''',
+            ),
+
+            # Text and tail and comment handling
+            (
+                r'''<div>Text<span>text</span>tail<!-- comment -->after comment<span>text</span>tail</div>''',
+                r'''<div>Text<span>text</span>tail<!-- comment -->after comment<span>text</span>tail</div>'''
+            ),
+
+            (
+                r'''<div><?my-pi attr1="val1" attr2="val2" ?></div>''',
+                r'''<div><?my-pi attr1="val1" attr2="val2" ?></div>'''
+            ),
+
+            # attribute escaping
+            (
+                r'''<div attr1="'foo>&amp;'">abc</div>''',
+                r'''<div attr1="'foo&gt;&amp;'">abc</div>''',
+            ),
+            # Always uses double quotes
+            (
+                r'''<div attr1='"foo"'>abc</div>''',
+                r'''<div attr1="&quot;foo&quot;">abc</div>''',
+            ),
+
+        )
+
+
+        serializer = XHTMLSerializer(xml_declaration=False)
+        for (test_in, test_out) in tests:
+            x = lxml.etree.fromstring(test_in)
+            s = serializer.write_element(x)
+            assert s == test_out
+
+            # Round trip
+            x2 = lxml.etree.fromstring(s)
+            assert serializer.write_element(x2) == s
+

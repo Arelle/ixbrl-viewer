d59e267154301b07a1b02c748a92daac4dff73e8
Paul Warren <pdw@ex-parrot.com>
2023-05-18 16:42:09 +0100

    Don't attempt to get prefix or units for footnotes.

--- iXBRLViewerPlugin/viewer/src/js/report.js
+++ iXBRLViewerPlugin/viewer/src/js/report.js
@@ -184,6 +184,7 @@ iXBRLReport.prototype.prefixMap = function() {
 iXBRLReport.prototype.getUsedPrefixes = function() {
     if (this._usedPrefixes === undefined) {
         this._usedPrefixes = new Set(Object.values(this._items)
+                .filter(f => f instanceof Fact)
                 .map(f => f.getConceptPrefix()));
     }
     return this._usedPrefixes;
@@ -196,6 +197,7 @@ iXBRLReport.prototype.getUsedPrefixes = function() {
 iXBRLReport.prototype.getUsedUnits = function() {
     if (this._usedUnits === undefined) {
         this._usedUnits = new Set(Object.values(this._items)
+                .filter(f => f instanceof Fact)
                 .map(f => f.unit()?.value())
                 .filter(f => f)
                 .sort());

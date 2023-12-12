bd59d64ce7ac7bf73dc6d483611220e5a1c39bd4
Paul Warren <pdw@ex-parrot.com>
2023-12-12 22:27:14 +0000

    Modernize interval.js

--- iXBRLViewerPlugin/viewer/src/js/interval.js
+++ iXBRLViewerPlugin/viewer/src/js/interval.js
@@ -14,59 +14,60 @@
 
 import Decimal from 'decimal.js';
 
-export function Interval(a, b) {
-    this.a = typeof a == 'object' ? a : new Decimal(a);
-    this.b = typeof b == 'object' ? b : new Decimal(b);
-}
+export class Interval {
+    constructor(a, b) {
+        this.a = typeof a == 'object' ? a : new Decimal(a);
+        this.b = typeof b == 'object' ? b : new Decimal(b);
+    }
 
-Interval.fromFact = function(fact) {
-    if (!fact.isNumeric()) {
-        return undefined;
-    }
-    const decimals = fact.decimals();
-    let width = 0;
-    if (decimals !== undefined) {
-        const x = new Decimal(10);
-        width = x.pow(0-decimals).times(0.5);
-    }
-    try {
-        const value = new Decimal(fact.value());
-        return new Interval(value.minus(width), value.plus(width));
-    } catch (e) {
-        if (e instanceof Error && /DecimalError/.test(e.message)) {
+    static fromFact(fact) {
+        if (!fact.isNumeric()) {
             return undefined;
         }
-        throw e;
+        const decimals = fact.decimals();
+        let width = 0;
+        if (decimals !== undefined) {
+            const x = new Decimal(10);
+            width = x.pow(0-decimals).times(0.5);
+        }
+        try {
+            const value = new Decimal(fact.value());
+            return new Interval(value.minus(width), value.plus(width));
+        } catch (e) {
+            if (e instanceof Error && /DecimalError/.test(e.message)) {
+                return undefined;
+            }
+            throw e;
+        }
+    }
+
+    intersection(other) {
+        return Interval.intersection(this, other);
+    }
+
+    static intersection(...intervals) {
+        if (intervals.includes(undefined) || intervals.length == 0) {
+            return undefined;
+        }
+        const aa = intervals.map(x => x.a);
+        const bb = intervals.map(x => x.b);
+        const a = Decimal.max(...aa);
+        const b = Decimal.min(...bb);
+        if (b.lessThan(a)) {
+            return undefined;
+        }
+        return new Interval(a, b);
+    }
+
+    plus(other) {
+        return new Interval(this.a.plus(other.a), this.b.plus(other.b));
+    }
+
+    times(x) {
+        return x > 0 ? new Interval(this.a.times(x), this.b.times(x)) : new Interval(this.b.times(x), this.a.times(x));
+    }
+
+    midpoint() {
+        return Decimal.add(this.a, this.b).div(2);
     }
 }
-
-Interval.prototype.intersection = function(other) {
-    return Interval.intersection(this, other);
-}
-
-Interval.intersection = function(...intervals) {
-    if (intervals.includes(undefined) || intervals.length == 0) {
-        return undefined;
-    }
-    const aa = intervals.map(x => x.a);
-    const bb = intervals.map(x => x.b);
-    const a = Decimal.max(...aa);
-    const b = Decimal.min(...bb);
-    if (b.lessThan(a)) {
-        return undefined;
-    }
-    return new Interval(a, b);
-}
-
-Interval.prototype.plus = function(other) {
-    return new Interval(this.a.plus(other.a), this.b.plus(other.b));
-}
-
-Interval.prototype.times = function(x) {
-    return x > 0 ? new Interval(this.a.times(x), this.b.times(x)) : new Interval(this.b.times(x), this.a.times(x));
-}
-
-Interval.prototype.midpoint = function() {
-    return Decimal.add(this.a, this.b).div(2);
-}
-

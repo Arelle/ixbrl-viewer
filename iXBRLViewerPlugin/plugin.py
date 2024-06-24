d5cccaae82b27815ca657f736de7b7ad8d393bb9
Paul Warren <pdw@ex-parrot.com>
2024-06-24 22:04:24 +0100

    Fix type checking issue.

--- iXBRLViewerPlugin/plugin.py
+++ iXBRLViewerPlugin/plugin.py
@@ -2,6 +2,6 @@ from arelle.utils.PluginData import PluginData
 from .iXBRLViewer import IXBRLViewerBuilder
 from typing import Optional
 
-class IXBRLViewerPluginData(PluginData):
+class IXBRLViewerPluginData(PluginData): # type: ignore[misc]
 
     builder: Optional[IXBRLViewerBuilder] = None

ff3f9f2437688652f413ffd8dd1336ab82e6dab6
Paul Warren <pdw@ex-parrot.com>
2024-09-28 23:23:42 +0100

    Redesign inspector headers.

--- iXBRLViewerPlugin/viewer/src/js/inspector.js
+++ iXBRLViewerPlugin/viewer/src/js/inspector.js
@@ -101,7 +101,7 @@ export class Inspector {
                 $("#inspector .controls .outline-button").on("click", function () {
                     $(this).closest("#inspector").removeClass(["summary-mode", "search-mode"]).toggleClass("outline-mode");
                 });
-                $("#inspector-head .back").on("click", function () {
+                $("#inspector .back").on("click", function () {
                     $(this).closest("#inspector").removeClass(["summary-mode", "outline-mode", "search-mode"]);
                 });
                 $(".popup-trigger").on("mouseenter", function () {

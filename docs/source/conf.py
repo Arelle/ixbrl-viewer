ab9f63204260766c64fece149fa17e8221cddaa8
Aaron Cameron <aaron.cameron@workiva.com>
2025-02-10 11:11:29 -0700

    Initialize RTD configuration, user guide

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Arelle iXBRL Viewer"
+copyright = "2011-present Workiva, Inc."
+author = "support@arelle.org"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#ac218e",
+        "color-brand-content": "#ac218e",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#6ecacb",
+        "color-brand-content": "#6ecacb",
+    },
+}
+html_title = "Arelle iXBRL Viewer <release>"
+html_favicon = "_images/favicon.ico"
+html_logo = "_images/arelle.png"
+
+pygments_style = "xcode"
+pygments_dark_style = "monokai"
---
+++ docs/source/index.rst
@@ -0,0 +1,8 @@
+Arelle iXBRL Viewer Documentation
+=================================
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+
+   user_guides/user_guide

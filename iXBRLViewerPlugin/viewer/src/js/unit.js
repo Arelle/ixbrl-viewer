47132644ba3772790166683ffca2a0aac2533f64
Stuart Rowan <stuart.rowan@strr.org>
2025-02-19 21:25:21 +0000

    Adjust unit display code to use a symbol in preference to the unit name, rather than only preferring the symbol when the measure is a currency.
    Should mean we get units of m² and m³ not sqm and m3

--- iXBRLViewerPlugin/viewer/src/js/unit.js
+++ iXBRLViewerPlugin/viewer/src/js/unit.js
@@ -12,18 +12,19 @@ import { utr } from "./utr";
 function measureLabel(report, measure) {
     const qname = report.qname(measure);
     if (qname.namespace === NAMESPACE_ISO4217) {
-        // Prefer a name from our own i18n resources
+        // Prefer a name from our own i18n resources for currencies
         const keyi18n = `currencies:unitFormat${qname.localname}`;
         if (i18next.exists(keyi18n)) {
             return i18next.t(keyi18n);
         }
-        // Fall back on symbol from UTR ...
-        const utrEntry = utr.get(qname);
-        if (utrEntry !== undefined) {
-            // ... but disambiguate "$" symbol
-            return utrEntry.symbol == '$' ? `${qname.localname} $` : utrEntry.symbol;
-        }
     }
+    // Pick symbol from UTR ...
+    const utrEntry = utr.get(qname);
+    if (utrEntry !== undefined && utrEntry.symbol !== undefined) {
+        // ... but disambiguate "$" symbol
+        return utrEntry.symbol == '$' ? `${qname.localname} $` : utrEntry.symbol;
+    }
+    // Otherwise fallback to unitId 
     if (measure.includes(':')) {
         return measure.split(':')[1];
     }

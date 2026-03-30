f40f207dea495053e03b5edb5159c71b1aa4dc7a
Austin M. Matherne <Austin.Matherne@Workiva.com>
2026-03-29 21:28:17 -0400

    Remove showSupportNotice option
    Option no longer needed or supported

--- iXBRLViewerPlugin/viewer/src/js/inspector.js
+++ iXBRLViewerPlugin/viewer/src/js/inspector.js
@@ -61,7 +61,6 @@ export class Inspector {
             returnEmptyString: false,
             fallbackLng: 'en',
             debug: false,
-            showSupportNotice: false,
             resources: 
                 Object.fromEntries(
                   langs.map(l => [l, Object.fromEntries(
